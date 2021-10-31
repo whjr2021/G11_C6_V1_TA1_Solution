bb678b85a803186c09c47ee219823c7faff71fbe
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-31 11:34:41 +0530

    Add files via upload

---
+++ C6_TA1_Solution.py
@@ -0,0 +1,14 @@
+# TA 1a: Create a Python list with elements 12, 56, 90 and name it as "roll_no".
+roll_no = [12, 56, 90]
+print(roll_no)
+
+# TA 1b: Append the elements 67 and 39 to the "roll_no" list.
+roll_no.append(67)
+print(roll_no)
+
+roll_no.append(39)
+print(roll_no)
+
+# TA 1c: Remove the element 90 from "roll_no" list.
+roll_no.remove(90)
+print(roll_no)
